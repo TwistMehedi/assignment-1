715317817c1067e9cc7906482f2b6a9f1d0f1efa
TwistMehedi <ataul1708@gmail.com>
2025-11-16 23:40:55 +0600

    solved problem number 2

--- solution.ts
+++ solution.ts
@@ -13,6 +13,22 @@ const formatValue = <T extends string | number | boolean>(value: T): string | nu
     return value;
 };
 
-console.log(formatValue("hello"));
+console.log(formatValue('hello'));
 console.log(formatValue(5));
 console.log(formatValue(true));
+
+// problem number - 2
+
+const getLength = (value: string | any[]): number =>{
+
+    if(typeof value === "string"){
+        return value.length
+    }else if(Array.isArray(value)){
+        return value.length
+    }
+
+    return 0;
+};
+
+console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
